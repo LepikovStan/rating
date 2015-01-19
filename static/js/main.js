bd00ca1d0a6e1b00725045495a6d9f5d79267567
Lepikov Stanislav <s.lepikov@semrush.com>
2015-01-19 15:16:24 +0300

    m

--- static/js/main.js
+++ static/js/main.js
@@ -127,13 +127,14 @@ var select = '<select>'+
 			
 			_.each(this.model.get('games'), function(game, num) {
 				var scores = '';
-			
-				_.each(game.winner.score, function(score, i) {
-					scores += '<div>' + game.winner.score[i] + ':' + game.loser.score[i] + '</div>';
-				});
-			
-				html += '<table class="match"><tr><td>'+game.winner.fullname+'</td><td>'+scores+'</td><td>'+game.loser.fullname+'</td></tr></table>';
-				
+
+				if (game.format === 'single') {
+					_.each(game.winner.score, function (score, i) {
+						scores += '<div>' + game.winner.score[i] + ':' + game.loser.score[i] + '</div>';
+					});
+
+					html += '<table class="match"><tr><td>' + game.winner.fullname + '</td><td>' + scores + '</td><td>' + game.loser.fullname + '</td></tr></table>';
+				}
 				console.log('game', game)
 			});
 		
@@ -163,24 +164,13 @@ var select = '<select>'+
 				loser,
 				K;
 			
-			$('select').each(function(i, el) {
-				if ($(this).closest('.pl1').length) {
-					score1.push($(this).val());
-				}
-				if ($(this).closest('.pl2').length) {
-					score2.push($(this).val());
-				}
-			});
-			
 			if (player3 && player4) {
-				console.log('double1: ' + player1.fullname + '/' + player2.fullname + ', rating: ' + this.getDoubleRating(parseInt(player1.rating), parseInt(player2.rating)));
-				console.log('double2: ' + player3.fullname + '/' + player4.fullname + ', rating: ' + this.getDoubleRating(parseInt(player3.rating), parseInt(player4.rating)));
+				this.sendDouble();
 			} else {
-				console.log('player1: ' + player1.fullname + ', rating: ' + player1.rating);
-				console.log('player2: ' + player2.fullname + ', rating: ' + player2.rating);
+				this.sendSingle();
 			}
 			
-			_.each(score1, function(el, i) {
+			/*_.each(score1, function(el, i) {
 				if (score1[i] > score2[i]) {
 					win1 += 1;
 				}
@@ -211,16 +201,16 @@ var select = '<select>'+
 				1, 
 				T, 
 				this.model.get('K')[K],
-				winner,
-				loser
-			),
+				winner.rating,
+				loser.rating
+			);
 			loserdiff = this.newRating(
 				-1, 
 				1, 
 				T, 
 				this.model.get('K')[K],
-				winner,
-				loser
+				winner.rating,
+				loser.rating
 			);
 					
 			
@@ -258,6 +248,227 @@ var select = '<select>'+
 			});
 			
 			this.renderTable();
+			this.renderGames();*/
+		},
+		sendDouble: function() {
+			var score1 = [],
+				score2 = [],
+				win1 = 0,
+				win2 = 0,
+				player1 = this.model.get('users')[$('select[name="player1"]').val()],
+				player2 = this.model.get('users')[$('select[name="player2"]').val()],
+				player3 = this.model.get('users')[$('select[name="player3"]').val()],
+				player4 = this.model.get('users')[$('select[name="player4"]').val()],
+				tournamentInfo = this.model.get('tournamentInfo'),
+				T = tournamentInfo[$('select[name="tournamentInfo"]').val()].coeff,
+				D = 1,
+				winnerRating,
+				loserRating,
+				winnerscore,
+				loserscore,
+				K;
+
+			console.log('double1: ' + player1.fullname + '/' + player2.fullname + ', rating: ' + this.getDoubleRating(parseInt(player1.rating), parseInt(player2.rating)));
+			console.log('double2: ' + player3.fullname + '/' + player4.fullname + ', rating: ' + this.getDoubleRating(parseInt(player3.rating), parseInt(player4.rating)));
+
+			$('select').each(function(i, el) {
+				if ($(this).closest('.pl1').length) {
+					score1.push($(this).val());
+				}
+				if ($(this).closest('.pl2').length) {
+					score2.push($(this).val());
+				}
+			});
+
+			_.each(score1, function(el, i) {
+				if (score1[i] > score2[i]) {
+					win1 += 1;
+				}
+
+				if (score2[i] > score1[i]) {
+					win2 += 1;
+				}
+			});
+
+			if (win1 > win2) {
+				K = win1 - win2;
+				winnerscore = score1;
+				loserscore = score2;
+				winnerRating = this.getDoubleRating(parseInt(player1.rating), parseInt(player2.rating));
+				loserRating = this.getDoubleRating(parseInt(player3.rating), parseInt(player4.rating));
+			} else {
+				K = win2 - win1;
+				winnerscore = score2;
+				loserscore = score1;
+				winnerRating = this.getDoubleRating(parseInt(player3.rating), parseInt(player4.rating));
+				loserRating = this.getDoubleRating(parseInt(player1.rating), parseInt(player2.rating));
+			}
+
+			var winnerdiff = this.newRating(
+				1,
+				1,
+				T,
+				this.model.get('K')[K],
+				winnerRating,
+				loserRating
+			);
+			var loserdiff = this.newRating(
+				-1,
+				1,
+				T,
+				this.model.get('K')[K],
+				winnerRating,
+				loserRating
+			);
+
+			if (win1 > win2) {
+				console.log('winner: ' + player1.fullname + ', new rating: ' +
+					(parseInt(player1.rating) + winnerdiff) + ', diff: ' + winnerdiff
+				);
+				console.log('winner: ' + player2.fullname + ', new rating: ' +
+					(parseInt(player2.rating) + winnerdiff) + ', diff: ' + winnerdiff
+				);
+				console.log('loser: ' + player3.fullname + ', new rating: ' +
+					(parseInt(player3.rating) + loserdiff) + ', diff: ' + loserdiff
+				);
+				console.log('loser: ' + player4.fullname + ', new rating: ' +
+					(parseInt(player4.rating) + loserdiff) + ', diff: ' + loserdiff
+				);
+
+
+				player1.rating = parseInt(player1.rating) + winnerdiff;
+				player2.rating = parseInt(player2.rating) + winnerdiff;
+				player3.rating = parseInt(player3.rating) + loserdiff;
+				player4.rating = parseInt(player4.rating) + loserdiff;
+
+				player1.diff += winnerdiff;
+				player2.diff += winnerdiff;
+				player3.diff += loserdiff;
+				player4.diff += loserdiff;
+			} else {
+				console.log('winner: ' + player3.fullname + ', new rating: ' +
+					(parseInt(player3.rating) + winnerdiff) + ', diff: ' + winnerdiff
+				);
+				console.log('winner: ' + player4.fullname + ', new rating: ' +
+					(parseInt(player4.rating) + winnerdiff) + ', diff: ' + winnerdiff
+				);
+				console.log('loser: ' + player1.fullname + ', new rating: ' +
+					(parseInt(player1.rating) + loserdiff) + ', diff: ' + loserdiff
+				);
+				console.log('loser: ' + player2.fullname + ', new rating: ' +
+					(parseInt(player2.rating) + loserdiff) + ', diff: ' + loserdiff
+				);
+
+				player3.rating = parseInt(player3.rating) + winnerdiff;
+				player4.rating = parseInt(player4.rating) + winnerdiff;
+				player1.rating = parseInt(player1.rating) + loserdiff;
+				player2.rating = parseInt(player2.rating) + loserdiff;
+
+				player3.diff += winnerdiff;
+				player4.diff += winnerdiff;
+				player1.diff += loserdiff;
+				player2.diff += loserdiff;
+			}
+
+
+			this.renderTable();
+		},
+		sendSingle: function() {
+			var score1 = [],
+				score2 = [],
+				win1 = 0,
+				win2 = 0,
+				player1 = this.model.get('users')[$('select[name="player1"]').val()],
+				player2 = this.model.get('users')[$('select[name="player2"]').val()],
+				tournamentInfo = this.model.get('tournamentInfo'),
+				T = tournamentInfo[$('select[name="tournamentInfo"]').val()].coeff,
+				D = 1,
+				winner,
+				loser,
+				winnerscore,
+				loserscore,
+				K;
+
+			console.log('player1: ' + player1.fullname + ', rating: ' + player1.rating);
+			console.log('player2: ' + player2.fullname + ', rating: ' + player2.rating);
+
+			$('select').each(function(i, el) {
+				if ($(this).closest('.pl1').length) {
+					score1.push($(this).val());
+				}
+				if ($(this).closest('.pl2').length) {
+					score2.push($(this).val());
+				}
+			});
+
+			_.each(score1, function(el, i) {
+				if (score1[i] > score2[i]) {
+					win1 += 1;
+				}
+
+				if (score2[i] > score1[i]) {
+					win2 += 1;
+				}
+			});
+
+			if (win1 > win2) {
+				K = win1 - win2;
+				winner = player1;
+				loser = player2;
+				winnerscore = score1;
+				loserscore = score2;
+			} else {
+				K = win2 - win1;
+				winner = player2;
+				loser = player1;
+				winnerscore = score2;
+				loserscore = score1;
+			}
+
+			var winnerdiff = this.newRating(
+				1,
+				1,
+				T,
+				this.model.get('K')[K],
+				winner.rating,
+				loser.rating
+			);
+			var loserdiff = this.newRating(
+				-1,
+				1,
+				T,
+				this.model.get('K')[K],
+				winner.rating,
+				loser.rating
+			);
+			console.log('model K', this.model.get('K')[2], K)
+
+			winner.rating = parseInt(winner.rating) + winnerdiff;
+			loser.rating = parseInt(loser.rating) + loserdiff;
+
+			winner.diff += winnerdiff;
+			loser.diff += loserdiff;
+
+			this.model.setGame({
+				format: 'single',
+				winner: {
+					fullname: winner.fullname,
+					score: _.compact(winnerscore)
+				},
+				loser: {
+					fullname: loser.fullname,
+					score: _.compact(loserscore)
+				}
+			});
+
+			console.log('winner: ' + winner.fullname + ', new rating: ' +
+				(parseInt(winner.rating) + winnerdiff) + ', diff: ' + winnerdiff
+			);
+			console.log('loser: ' + loser.fullname + ', new rating: ' +
+				(parseInt(loser.rating) + loserdiff) + ', diff: ' + loserdiff
+			);
+
+			this.renderTable();
 			this.renderGames();
 		},
 		renderTable: function() {
@@ -279,15 +490,15 @@ var select = '<select>'+
 		
 			this.$('.ratingTable').html(_.template(this.ratingTableTemplate)(this.model.attributes));
 		},
-		newRating: function(W, D, T, K, winner, loser) {
-			//console.log('!!!!! newRating !!!!!!', winner.fullname, loser.fullname);
+		newRating: function(W, D, T, K, winnerRating, loserRating) {
+			//console.log('!!!!! newRating !!!!!!', loserRating, winnerRating);
 			//console.log('WDTK', W, D, T, K, W*D*T*K);
-			//console.log('RL - RW', loser.rating - winner.rating);
-			//console.log('RL - RW/400', (loser.rating - winner.rating)/400);
-			//console.log('pow RL - RW/400', Math.pow(10, (loser.rating - winner.rating)/400));
-			//console.log('1 - 1/10 (RL - RW/400) + 1', 1 - (1/(Math.pow(10, (loser.rating - winner.rating)/400) + 1)));
+			//console.log('RL - RW', loserRating - winnerRating);
+			//console.log('RL - RW/400', (loserRating - winnerRating)/400);
+			//console.log('pow RL - RW/400', Math.pow(10, (loserRating - winnerRating)/400));
+			//console.log('1 - 1/10 (RL - RW/400) + 1', 1 - (1/(Math.pow(10, (loserRating - winnerRating)/400) + 1)));
 			
-			return Math.round(W*D*T*K * (1 - (1/(Math.pow(10, (loser.rating - winner.rating)/400) + 1))));
+			return Math.round(W*D*T*K * (1 - (1/(Math.pow(10, (loserRating - winnerRating)/400) + 1))));
 		},
 		changePlayer: function(e) {
 			var $this = $(e.target),
